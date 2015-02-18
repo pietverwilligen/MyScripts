0f09375eb46adc1dd16480132b68e4a0d6b782db
Piet Verwilligen <piet@blacksad.(none)>
2015-02-18 14:15:33 +0100

    das-queries

---
+++ das-queries/DAS_Examples_DisplacedMuons_AllExecuted.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo ""
+echo "python das_client.py --query=\"dataset=/*DarkSUSY*mGammaD_0400*14TeV*/dildick*DIGI_RECO*/* instance=prod/phys03\" --limit=100"
+echo "---------------------------------------------------------------------------------------------------------------------------"
+python das_client.py --query="dataset=/*DarkSUSY*mGammaD_0400*14TeV*/dildick*DIGI_RECO*/* instance=prod/phys03" --limit=100
+echo "---------------------------------------------------------------------------------------------------------------------------"
+echo ""
+echo "python das_client.py --query=\"dataset=/*DarkSUSY*mGammaD_20000*14TeV*/dildick*DIGI_RECO*/* instance=prod/phys03\" --limit=100"
+echo "---------------------------------------------------------------------------------------------------------------------------"
+python das_client.py --query="dataset=/*DarkSUSY*mGammaD_20000*14TeV*/dildick*DIGI_RECO*/* instance=prod/phys03" --limit=100
+echo "---------------------------------------------------------------------------------------------------------------------------"
+echo ""
+echo "python das_client.py --query=\"site dataset=/DarkSUSY_mH_125_mGammaD_0400_ctau_05_14TeV_madgraph452_bridge224_LHE_pythia6_GEN_SIM/dildick-DarkSUSY_mH_125_mGammaD_0400_ctau_05_14TeV_madgraph452_bridge224_LHE_pythia6_DIGI_RECO-90998fdfabd32352e84b8d7ca7f654f1/USER instance=prod/phys03\" --limit=100"
+echo "---------------------------------------------------------------------------------------------------------------------------"
+python das_client.py --query="site dataset=/DarkSUSY_mH_125_mGammaD_0400_ctau_05_14TeV_madgraph452_bridge224_LHE_pythia6_GEN_SIM/dildick-DarkSUSY_mH_125_mGammaD_0400_ctau_05_14TeV_madgraph452_bridge224_LHE_pythia6_DIGI_RECO-90998fdfabd32352e84b8d7ca7f654f1/USER instance=prod/phys03" --limit=100
+echo "---------------------------------------------------------------------------------------------------------------------------"
+echo ""
+echo "python das_client.py --query=\"file dataset=/DarkSUSY_mH_125_mGammaD_0400_ctau_05_14TeV_madgraph452_bridge224_LHE_pythia6_GEN_SIM/dildick-DarkSUSY_mH_125_mGammaD_0400_ctau_05_14TeV_madgraph452_bridge224_LHE_pythia6_DIGI_RECO-90998fdfabd32352e84b8d7ca7f654f1/USER instance=prod/phys03\" --limit=100"
+echo "---------------------------------------------------------------------------------------------------------------------------"
+python das_client.py --query="file dataset=/DarkSUSY_mH_125_mGammaD_0400_ctau_05_14TeV_madgraph452_bridge224_LHE_pythia6_GEN_SIM/dildick-DarkSUSY_mH_125_mGammaD_0400_ctau_05_14TeV_madgraph452_bridge224_LHE_pythia6_DIGI_RECO-90998fdfabd32352e84b8d7ca7f654f1/USER instance=prod/phys03" --limit=100
+echo "---------------------------------------------------------------------------------------------------------------------------"
+echo ""
+echo "python das_client.py --query=\"dataset=/*DarkSUSY*mGammaD_20000*14TeV*/dildick*DIGI_RECO*PU140*/* instance=prod/phys03\" --limit=100"
+echo "---------------------------------------------------------------------------------------------------------------------------"
+python das_client.py --query="dataset=/*DarkSUSY*mGammaD_20000*14TeV*/dildick*DIGI_RECO*PU140*/* instance=prod/phys03" --limit=100
+echo "---------------------------------------------------------------------------------------------------------------------------"
+echo ""
+echo "python das_client.py --query=\"file dataset=/DarkSUSY_mH_125_mGammaD_20000_ctau_50_14TeV_madgraph452_bridge224_LHE_pythia6_GEN_SIM_2/dildick-DarkSUSY_mH_125_mGammaD_20000_ctau_50_14TeV_madgraph452_bridge224_LHE_pythia6_DIGI_RECO_v4_PU140-10db75cd34e147826f192372714ebcfd/USER instance=prod/phys03\" --limit=100"
+echo "---------------------------------------------------------------------------------------------------------------------------"
+python das_client.py --query="file dataset=/DarkSUSY_mH_125_mGammaD_20000_ctau_50_14TeV_madgraph452_bridge224_LHE_pythia6_GEN_SIM_2/dildick-DarkSUSY_mH_125_mGammaD_20000_ctau_50_14TeV_madgraph452_bridge224_LHE_pythia6_DIGI_RECO_v4_PU140-10db75cd34e147826f192372714ebcfd/USER  instance=prod/phys03" --limit=100
+echo "---------------------------------------------------------------------------------------------------------------------------"
+echo ""
